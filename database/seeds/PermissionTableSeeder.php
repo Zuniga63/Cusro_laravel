8cf6645af048dc3ee1f4c03b32b759bb22553395
Andres Zuñiga <andres.zuniga.063@gmail.com>
2020-09-24 10:41:06 -0500

    Permission seeder now directly

--- database/seeds/PermissionTableSeeder.php
+++ database/seeds/PermissionTableSeeder.php
@@ -1,19 +1,31 @@
 <?php
 
-use App\Models\Permission;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PermissionTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //Despues de crear el modelo y el factory
-        //Lo que se hace es invocar el modelo
-        factory(Permission::class, 100)->create();
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    //Se crean los roles de la table rol
+    $rols = [
+      'Crear permisos',
+      'Eliminar permisos',
+      'Crear roles'
+    ];
+    //Ahora se crean los datos usando DB
+    foreach ($rols as $key => $value) {
+      DB::table('permission')->insert([
+        'name' => $value,
+        'slug' => str_replace(' ', "_", mb_strtolower($value)),
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+      ]);
+    } //end of foreach()
+  }
 }
