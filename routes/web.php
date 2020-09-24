a0885d4240176a6ebf2ff2ca2ad37fa3fd6e4803
Andres Zuñiga <andres.zuniga.063@gmail.com>
2020-09-24 16:44:24 -0500

    group rooute was create

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'InicioController@index');
-Route::get('admin/permiso', 'admin\PermissionController@index')->name('permission');
-Route::get('admin/permiso/crear', 'admin\PermissionController@create')->name('create_permission');
-Route::get('admin/menu', 'admin\MenuController@index')->name('permission');
-Route::get('admin/menu/crear', 'admin\MenuController@create')->name('permission');
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function(){
+  Route::get('permiso', 'PermissionController@index')->name('permission');
+  Route::get('permiso/crear', 'PermissionController@create')->name('create_permission');
+
+  Route::get('menu', 'MenuController@index')->name('menu');
+  Route::post('menu', 'MenuController@store')->name('store_menu');
+  Route::get('menu/crear', 'MenuController@create')->name('create_menu');
+});
+
+/**
+ * Lo siguiente es un sistema de ruta cacheable
+ */
+
+// Route::get('admin/permiso', 'admin\PermissionController@index')->name('permission');
+// Route::get('admin/permiso/crear', 'admin\PermissionController@create')->name('create_permission');
+
+// Route::get('admin/menu', 'admin\MenuController@index')->name('permission');
+// Route::get('admin/menu/crear', 'admin\MenuController@create')->name('permission');
 
 
 // Route::view('/', 'welcome');
